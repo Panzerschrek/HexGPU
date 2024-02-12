73220de2161b544e093d15ebf7d597e289262f2e
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-02-12 11:56:13 +0300

    Generirujem vse storony blokov.

--- src/shaders/geometry_gen.comp.glsl
+++ src/shaders/geometry_gen.comp.glsl
@@ -45,10 +45,11 @@ layout(binding= 2, std430) buffer chunk_data_buffer
 const int c_indices_per_quad= 6;
 
 const int c_chunk_width_log2= 4;
+const int c_chunk_height_log2= 7;
 
 int ChunkBlockAddress(int x, int y, int z)
 {
-	return z + (y << c_chunk_width_log2) + (x << (c_chunk_width_log2 * 2));
+	return z + (y << c_chunk_height_log2) + (x << (c_chunk_width_log2 + c_chunk_height_log2));
 }
 
 void main()
@@ -69,7 +70,12 @@ void main()
 	int base_y= 2 * block_global_y - (block_global_x & 1) + 1;
 
 	int block_address_in_chunk= ChunkBlockAddress( block_local_x, block_local_y, z );
-	if( chunk_data[ block_address_in_chunk ] != chunk_data[ block_address_in_chunk + 1 ] )
+	int block_address_up= block_address_in_chunk + 1;
+	int block_address_north= block_address_in_chunk + (1 << c_chunk_height_log2);
+	int block_address_north_east= block_address_in_chunk + (((block_local_x + 1) & 1) << (c_chunk_height_log2)) + (1 <<(c_chunk_width_log2 + c_chunk_height_log2));
+	int block_address_south_east= block_address_north_east - (1 << c_chunk_height_log2);
+
+	if( chunk_data[ block_address_in_chunk ] != chunk_data[ block_address_up ] )
 	{
 		// Add two hexagon quads.
 		uint prev_index_count= atomicAdd(command.indexCount, c_indices_per_quad * 2);
@@ -123,7 +129,7 @@ void main()
 		}
 	}
 
-	if( chunk_data[ block_address_in_chunk ] != chunk_data[ block_address_in_chunk + (1 << (c_chunk_width_log2 * 2)) ] )
+	if( chunk_data[ block_address_in_chunk ] != chunk_data[ block_address_north ] )
 	{
 		// Add north quad.
 		uint prev_index_count= atomicAdd(command.indexCount, c_indices_per_quad);
@@ -135,6 +141,64 @@ void main()
 		quad.vertices[1].pos= i16vec4(int16_t(base_x + 2), int16_t(base_y + 2), int16_t(z + 1), 0.0);
 		quad.vertices[2].pos= i16vec4(int16_t(base_x), int16_t(base_y + 2), int16_t(z + 1), 0.0);
 		quad.vertices[3].pos= i16vec4(int16_t(base_x), int16_t(base_y + 2), int16_t(z), 0.0);
+
+		quad.vertices[0].color[0]= uint8_t((invocation.x + 1) * 32);
+		quad.vertices[0].color[1]= uint8_t((invocation.y + 1) * 32);
+		quad.vertices[0].color[2]= uint8_t(0);
+		quad.vertices[1].color[0]= uint8_t(invocation.x * 32);
+		quad.vertices[1].color[1]= uint8_t((invocation.y + 1) * 32);
+		quad.vertices[1].color[2]= uint8_t(0);
+		quad.vertices[2].color[0]= uint8_t(invocation.x * 32);
+		quad.vertices[2].color[1]= uint8_t(invocation.y * 32);
+		quad.vertices[2].color[2]= uint8_t(0);
+		quad.vertices[3].color[0]= uint8_t((invocation.x + 1) * 32);
+		quad.vertices[3].color[1]= uint8_t(invocation.y * 32);
+		quad.vertices[3].color[2]= uint8_t(0);
+
+		quads[quad_index]= quad;
+	}
+
+	if( chunk_data[ block_address_in_chunk ] != chunk_data[ block_address_north_east ] )
+	{
+		// Add north-east quad.
+		uint prev_index_count= atomicAdd(command.indexCount, c_indices_per_quad);
+		uint quad_index= prev_index_count / 6;
+
+		Quad quad;
+
+		quad.vertices[0].pos= i16vec4(int16_t(base_x + 3), int16_t(base_y + 1), int16_t(z), 0.0);
+		quad.vertices[1].pos= i16vec4(int16_t(base_x + 3), int16_t(base_y + 1), int16_t(z + 1), 0.0);
+		quad.vertices[2].pos= i16vec4(int16_t(base_x + 2), int16_t(base_y + 2), int16_t(z + 1), 0.0);
+		quad.vertices[3].pos= i16vec4(int16_t(base_x + 2), int16_t(base_y + 2), int16_t(z), 0.0);
+
+		quad.vertices[0].color[0]= uint8_t((invocation.x + 1) * 32);
+		quad.vertices[0].color[1]= uint8_t((invocation.y + 1) * 32);
+		quad.vertices[0].color[2]= uint8_t(0);
+		quad.vertices[1].color[0]= uint8_t(invocation.x * 32);
+		quad.vertices[1].color[1]= uint8_t((invocation.y + 1) * 32);
+		quad.vertices[1].color[2]= uint8_t(0);
+		quad.vertices[2].color[0]= uint8_t(invocation.x * 32);
+		quad.vertices[2].color[1]= uint8_t(invocation.y * 32);
+		quad.vertices[2].color[2]= uint8_t(0);
+		quad.vertices[3].color[0]= uint8_t((invocation.x + 1) * 32);
+		quad.vertices[3].color[1]= uint8_t(invocation.y * 32);
+		quad.vertices[3].color[2]= uint8_t(0);
+
+		quads[quad_index]= quad;
+	}
+
+	if( chunk_data[ block_address_in_chunk ] != chunk_data[ block_address_south_east ] )
+	{
+		// Add south-east quad.
+		uint prev_index_count= atomicAdd(command.indexCount, c_indices_per_quad);
+		uint quad_index= prev_index_count / 6;
+
+		Quad quad;
+
+		quad.vertices[0].pos= i16vec4(int16_t(base_x + 2), int16_t(base_y), int16_t(z), 0.0);
+		quad.vertices[1].pos= i16vec4(int16_t(base_x + 2), int16_t(base_y), int16_t(z + 1), 0.0);
+		quad.vertices[2].pos= i16vec4(int16_t(base_x + 3), int16_t(base_y + 1), int16_t(z + 1), 0.0);
+		quad.vertices[3].pos= i16vec4(int16_t(base_x + 3), int16_t(base_y + 1), int16_t(z), 0.0);
 
 		quad.vertices[0].color[0]= uint8_t((invocation.x + 1) * 32);
 		quad.vertices[0].color[1]= uint8_t((invocation.y + 1) * 32);

21bd8e037f6279a74a8a74b377a1770639cb65d5
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-02-14 15:34:32 +0300

    Zagružajem izobraženije iz fajla. Ispoljzujem dlä etogo biblioteku "stb_image".

--- src/WorldTexturesManager.cpp
+++ src/WorldTexturesManager.cpp
@@ -1,4 +1,22 @@
 #include "WorldTexturesManager.hpp"
+#include "Assert.hpp"
+#include <optional>
+#include <vector>
+
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#endif
+
+// This is single place, where this library included.
+// So, we can include implementation here.
+#define STB_IMAGE_IMPLEMENTATION
+#include "../stb/stb_image.h"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
 
 namespace HexGPU
 {
@@ -6,7 +24,9 @@ namespace HexGPU
 namespace
 {
 
-void FillTestImage(uint32_t width, uint32_t height, uint32_t* const data)
+using PixelType= uint32_t;
+
+void FillTestImage(uint32_t width, uint32_t height, PixelType* const data)
 {
 	for(uint32_t y= 0; y < height; ++y)
 	for(uint32_t x= 0; x < width; ++x)
@@ -15,13 +35,64 @@ void FillTestImage(uint32_t width, uint32_t height, uint32_t* const data)
 	}
 }
 
+struct Image
+{
+	uint32_t size[2];
+	std::vector<PixelType> data;
+};
+
+std::optional<Image> LoadImage(const char* const file_path)
+{
+	const std::string file_path_str(file_path);
+
+	int width, height, channels;
+	unsigned char* stbi_img_data= stbi_load(file_path_str.c_str(), &width, &height, &channels, 0);
+	if(stbi_img_data == nullptr)
+		return std::nullopt;
+
+	if(channels == 4)
+	{
+		const auto data_ptr= reinterpret_cast<const PixelType*>(stbi_img_data);
+
+		Image result;
+		result.size[0]= uint32_t(width);
+		result.size[1]= uint32_t(height);
+		result.data= std::vector<PixelType>(data_ptr, data_ptr + width * height);
+		stbi_image_free(stbi_img_data);
+		return result;
+	}
+	if( channels == 3)
+	{
+
+		Image result;
+		result.size[0]= uint32_t(width);
+		result.size[1]= uint32_t(height);
+		result.data.resize(width * height);
+
+		for(int i= 0; i < width * height; ++i)
+			result.data[i]=
+				(stbi_img_data[i*3  ]<< 0) |
+				(stbi_img_data[i*3+1]<< 8) |
+				(stbi_img_data[i*3+2]<<16) |
+				(255                << 24);
+
+		return result;
+	}
+
+	stbi_image_free(stbi_img_data);
+	return std::nullopt;
+}
+
 } // namespace
 
 WorldTexturesManager::WorldTexturesManager(WindowVulkan& window_vulkan)
 	: vk_device_(window_vulkan.GetVulkanDevice())
 {
-	const uint32_t c_texture_size_log2= 8;
-	const uint32_t c_texture_size= 1 << c_texture_size_log2;
+	const auto image= LoadImage("textures/brick.jpg");
+	HEX_ASSERT(image != std::nullopt);
+
+	(void) FillTestImage;
+
 	const uint32_t c_num_mips= 1; // TODO - create mips.
 
 	image_= vk_device_.createImageUnique(
@@ -29,7 +100,7 @@ WorldTexturesManager::WorldTexturesManager(WindowVulkan& window_vulkan)
 			vk::ImageCreateFlags(),
 			vk::ImageType::e2D,
 			vk::Format::eR8G8B8A8Unorm,
-			vk::Extent3D(c_texture_size, c_texture_size, 1u),
+			vk::Extent3D(image->size[0], image->size[1], 1u),
 			c_num_mips,
 			1u,
 			vk::SampleCountFlagBits::e1,
@@ -57,7 +128,7 @@ WorldTexturesManager::WorldTexturesManager(WindowVulkan& window_vulkan)
 
 	void* image_data_gpu_size= nullptr;
 	vk_device_.mapMemory(*image_memory_, 0u, memory_allocate_info.allocationSize, vk::MemoryMapFlags(), &image_data_gpu_size);
-	FillTestImage(c_texture_size, c_texture_size, reinterpret_cast<uint32_t*>(image_data_gpu_size));
+	std::memcpy(image_data_gpu_size, image->data.data(), image->size[0] * image->size[1] * sizeof(PixelType));
 	vk_device_.unmapMemory(*image_memory_);
 
 	image_view_= vk_device_.createImageViewUnique(

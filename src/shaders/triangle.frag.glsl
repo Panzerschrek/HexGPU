f9419e27f8775f3b95f69ffacc80c72212350326
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-02-14 15:23:43 +0300

    Masštabirujem teksturnyje koordinaty.

--- src/shaders/triangle.frag.glsl
+++ src/shaders/triangle.frag.glsl
@@ -9,8 +9,5 @@ layout(location = 0) out vec4 out_color;
 
 void main()
 {
-	//vec2 fract_lo= fract(f_tex_coord);
-	//vec2 fract_hi= fract(f_tex_coord / 8.0);
-	//out_color= vec4(fract_lo, abs(fract_hi.x - fract_hi.y), 1.0);
 	out_color= texture(texture_image, f_tex_coord);
 }
--- src/shaders/triangle.vert.glsl
+++ src/shaders/triangle.vert.glsl
@@ -11,8 +11,11 @@ layout(location=1) in vec2 tex_coord;
 layout(location= 0) out vec4 f_color;
 layout(location= 1) out vec2 f_tex_coord;
 
+// Texture coordnates are initialiiy somewhat scaled. Rescale them back.
+const vec2 c_tex_coord_scale= vec2(1.0 / 4.0, 1.0 / 4.0);
+
 void main()
 {
-	f_tex_coord= tex_coord;
+	f_tex_coord= tex_coord * c_tex_coord_scale;
 	gl_Position= view_matrix * vec4(pos, 1.0);
 }

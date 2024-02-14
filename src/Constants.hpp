4bf3b01ee82cdacf53c32602d1a6e0cb6569638c
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-02-14 08:48:50 +0300

    Pocinka sborki pod "MSVC".

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -4,17 +4,17 @@
 namespace HexGPU
 {
 
-const uint32_t c_chunk_width_log2= 4;
-const uint32_t c_chunk_width= 1 << c_chunk_width_log2;
+constexpr uint32_t c_chunk_width_log2= 4;
+constexpr uint32_t c_chunk_width= 1 << c_chunk_width_log2;
 
-const uint32_t c_chunk_height_log2= 7;
-const uint32_t c_chunk_height= 1 << c_chunk_height_log2;
+constexpr uint32_t c_chunk_height_log2= 7;
+constexpr uint32_t c_chunk_height= 1 << c_chunk_height_log2;
 
 // In blocks.
-const uint32_t c_chunk_volume= c_chunk_width * c_chunk_width * c_chunk_height;
+constexpr uint32_t c_chunk_volume= c_chunk_width * c_chunk_width * c_chunk_height;
 
 // TODO - make non-constant.
-const uint32_t c_chunk_matrix_size[2]{4, 4};
+constexpr const uint32_t c_chunk_matrix_size[2]{4, 4};
 
 inline uint32_t ChunkBlockAddress(const uint32_t x, const uint32_t y, const uint32_t z)
 {

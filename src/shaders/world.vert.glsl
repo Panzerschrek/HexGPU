35eb1e8d58f9f2e18d604d1a200bf9727d256d12
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-02-19 11:39:39 +0300

    Za scöt ispoljzovanija çelocislennyh veršinnyh atributov ubrany lišnije preobrazovanija dlä bitovyh manipuläçij so svetom.

--- src/shaders/world.vert.glsl
+++ src/shaders/world.vert.glsl
@@ -12,7 +12,7 @@ layout(push_constant) uniform uniforms_block
 };
 
 layout(location=0) in vec3 pos;
-layout(location=1) in vec4 tex_coord;
+layout(location=1) in i16vec4 tex_coord;
 
 layout(location= 0) out vec2 f_light;
 layout(location= 1) out vec2 f_tex_coord;
@@ -23,12 +23,12 @@ const vec2 c_tex_coord_scale= vec2(1.0 / 8.0, 1.0 / 4.0);
 
 void main()
 {
-	f_tex_coord= tex_coord.xy * c_tex_coord_scale;
-	f_tex_index= tex_coord.z;
+	f_tex_coord= vec2(tex_coord.xy) * c_tex_coord_scale;
+	f_tex_index= int(tex_coord.z);
 
 	// Use linear light function - using such function allows to lit more area.
-	f_light.x= float(int(tex_coord.w) & c_fire_light_mask) / 9.0 + 0.05;
-	f_light.y= float(int(tex_coord.w) >> c_sky_light_shift) / 16.0 + 0.05;
+	f_light.x= float(tex_coord.w & c_fire_light_mask) / 9.0 + 0.05;
+	f_light.y= float(tex_coord.w >> c_sky_light_shift) / 16.0 + 0.05;
 
 	gl_Position= view_matrix * vec4(pos, 1.0);
 }

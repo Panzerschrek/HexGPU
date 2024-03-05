de010292bbfd9fda9188c85ed19c4918bce4d916
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-03-05 09:11:05 +0300

    Ispoljzujem zerno so smescenijem dlä raznyh oktav šuma urovnä zemli.

--- src/shaders/world_gen.comp.glsl
+++ src/shaders/world_gen.comp.glsl
@@ -31,11 +31,12 @@ int GetGroundLevel(int global_x, int global_y)
 	int global_y_corrected= global_y - (global_x & 1);
 
 	// Add several octaves of triangle-interpolated noise.
+	// Use seed with offset to avoid fractal noise apperiance at world center (0, 0).
 	int noise=
-		(hex_TriangularInterpolatedNoiseDefault(global_y_corrected, global_x, seed, 6)     ) +
-		(hex_TriangularInterpolatedNoiseDefault(global_y_corrected, global_x, seed, 5) >> 1) +
-		(hex_TriangularInterpolatedNoiseDefault(global_y_corrected, global_x, seed, 4) >> 2) +
-		(hex_TriangularInterpolatedNoiseDefault(global_y_corrected, global_x, seed, 3) >> 3);
+		(hex_TriangularInterpolatedNoiseDefault(global_y_corrected, global_x, seed + 0, 6)     ) +
+		(hex_TriangularInterpolatedNoiseDefault(global_y_corrected, global_x, seed + 1, 5) >> 1) +
+		(hex_TriangularInterpolatedNoiseDefault(global_y_corrected, global_x, seed + 2, 4) >> 2) +
+		(hex_TriangularInterpolatedNoiseDefault(global_y_corrected, global_x, seed + 3, 3) >> 3);
 
 	// TODO - scale result noise depending on current biome.
 	int noise_scaled= noise >> 11;

c77778f9b9718896f5a40ec1873bdcea41e43b38
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-03-09 11:05:09 +0300

    Generirujem prostejšeje derevo.

--- src/Structures.cpp
+++ src/Structures.cpp
@@ -1,20 +1,60 @@
 #include "Structures.hpp"
+#include "Assert.hpp"
 
 namespace HexGPU
 {
 
+namespace
+{
+
+StructureDescription GenBasicTree(std::vector<BlockType>& out_data)
+{
+	StructureDescription structure;
+	structure.size[0]= 3;
+	structure.size[1]= 3;
+	structure.size[2]= 5;
+
+	const auto volume= structure.size[0] * structure.size[1] * structure.size[2];
+
+	structure.data_offset= uint32_t(out_data.size());
+	out_data.resize(out_data.size() + volume, BlockType::Air);
+
+	const auto set_block=
+		[&](const uint32_t x, const uint32_t y, const uint32_t z, const BlockType block_type)
+		{
+			HEX_ASSERT(x < structure.size[0]);
+			HEX_ASSERT(y < structure.size[1]);
+			HEX_ASSERT(z < structure.size[2]);
+			const uint32_t offset= z + y * structure.size[2] + x * (structure.size[2] * structure.size[1]);
+			out_data[structure.data_offset + offset]= block_type;
+		};
+
+	const uint32_t trunk_x= 1;
+	const uint32_t trunk_y= 1;
+	for(uint32_t z= 0; z < 4; ++z)
+	{
+		set_block(trunk_x, trunk_y, z, BlockType::Wood);
+
+		if(z >= 2)
+		{
+			set_block(trunk_x, trunk_y + 1, z, BlockType::Foliage);
+			set_block(trunk_x, trunk_y - 1, z, BlockType::Foliage);
+			set_block(trunk_x + 1, trunk_y + ((trunk_x + 1) & 1), z, BlockType::Foliage);
+			set_block(trunk_x + 1, trunk_y - (trunk_x & 1), z, BlockType::Foliage);
+			set_block(trunk_x - 1, trunk_y + ((trunk_x + 1) & 1), z, BlockType::Foliage);
+			set_block(trunk_x - 1, trunk_y - (trunk_x & 1), z, BlockType::Foliage);
+		}
+	}
+	set_block(trunk_x, trunk_y, 4, BlockType::Foliage);
+
+	return structure;
+}
+
+} // namespace
 Structures GenStructures()
 {
-	StructureDescription descriptiuon;
-	descriptiuon.size[0]= 2;
-	descriptiuon.size[1]= 3;
-	descriptiuon.size[2]= 4;
-
-	const auto volume= descriptiuon.size[0] * descriptiuon.size[1] * descriptiuon.size[2];
-
 	Structures structures;
-	structures.data.resize(structures.data.size() + volume, BlockType::Wood);
-	structures.descriptions.push_back(descriptiuon);
+	structures.descriptions.push_back(GenBasicTree(structures.data));
 
 	return structures;
 }
--- src/shaders/chunk_gen_prepare.comp.glsl
+++ src/shaders/chunk_gen_prepare.comp.glsl
@@ -24,8 +24,8 @@ void main()
 	ChunkGenInfo info;
 	info.num_structures= 1;
 
-	info.structures[0].min= i16vec4(3,  7, 60, 0);
-	info.structures[0].max= i16vec4(5, 10, 64, 0);
+	info.structures[0].min= i16vec4(4,  7, 60, 0);
+	info.structures[0].max= i16vec4(7, 10, 65, 0);
 
 	int chunk_index= chunk_position.x + chunk_position.y * world_size_chunks.x;
 	chunk_gen_infos[chunk_index]= info;

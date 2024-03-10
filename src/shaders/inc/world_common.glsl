ab7104a9f533343b5e091eea444dd67570238770
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-03-10 11:10:30 +0300

    Iz "Hex" perenesön kod šestiugoljnoj vyborki iz teksrury.

---
+++ src/shaders/inc/world_common.glsl
@@ -0,0 +1,60 @@
+const float c_hexagon_fetch_lod_edge= 0.5;
+
+ivec2 GetHexagonTexCoordY(vec2 tex_coord)
+{
+	vec2 tex_coord_dransformed;
+	vec2 tex_coord_transformed_floor;
+	ivec2 nearest_cell;
+	vec2 d;
+
+	tex_coord_dransformed.y= tex_coord.y;
+	tex_coord_transformed_floor.y= floor(tex_coord_dransformed.y);
+	nearest_cell.y= int(tex_coord_transformed_floor.y);
+	d.y= tex_coord_dransformed.y - tex_coord_transformed_floor.y;
+
+	tex_coord_dransformed.x= tex_coord.x - 0.5 * float((nearest_cell.y ^ 1) & 1);
+	tex_coord_transformed_floor.x= floor( tex_coord_dransformed.x );
+	nearest_cell.x= int( tex_coord_transformed_floor.x );
+	d.x= tex_coord_dransformed.x - tex_coord_transformed_floor.x;
+
+	if(d.x > 0.5 + 1.5 * d.y)
+		return ivec2(nearest_cell.x + ((nearest_cell.y ^ 1) & 1), nearest_cell.y - 1);
+
+	else
+	if(d.x < 0.5 - 1.5 * d.y)
+		return ivec2(nearest_cell.x - (nearest_cell.y & 1), nearest_cell.y - 1);
+
+	else
+		return nearest_cell;
+}
+
+vec4 HexagonFetch(in sampler2DArray tex, vec3 tex_coord)
+{
+	float lod= textureQueryLod(tex, tex_coord.xy).x;
+
+	if(lod <= c_hexagon_fetch_lod_edge)
+	{
+		ivec2 tex_size= textureSize( tex, 0 ).xy;
+		int texture_layer= int(tex_coord.z + 0.49);
+
+		return texelFetch(
+			tex,
+			ivec3(
+				mod(GetHexagonTexCoordY(tex_coord.xy * vec2(tex_size)) ,tex_size),
+				texture_layer),
+			0);
+	}
+	else
+		return texture(tex, tex_coord);
+}
+
+vec3 CombineLight(vec3 fire_light, vec3 sky_light, vec3 ambient_light)
+{
+	// Use special formula to make one light source weaker when other like source value is big.
+	const vec3 c_one= vec3(1.0, 1.0, 1.0);
+	const float c_mul= 0.28;
+	return
+		fire_light * (c_one - c_mul * sky_light) +
+		sky_light * (c_one - c_mul * fire_light) +
+		ambient_light;
+}
--- src/shaders/world.frag.glsl
+++ src/shaders/world.frag.glsl
@@ -1,5 +1,9 @@
 #version 450
 
+#extension GL_GOOGLE_include_directive : require
+
+#include "inc/world_common.glsl"
+
 layout(binding= 1) uniform sampler2DArray texture_image;
 
 layout(location= 0) in vec2 f_light;
@@ -13,20 +17,9 @@ const vec3 c_sky_light_color= vec3(0.95, 0.95, 1.0); // TODO - make depending on
 // const vec3 c_sky_light_color= vec3(0.2, 0.2, 0.2);
 const vec3 c_ambient_light_color= vec3(0.06f, 0.06f, 0.06f);
 
-vec3 CombineLight(vec3 fire_light, vec3 sky_light, vec3 ambient_light)
-{
-	// Use special formula to make one light source weaker when other like source value is big.
-	const vec3 c_one= vec3(1.0, 1.0, 1.0);
-	const float c_mul= 0.28;
-	return
-		fire_light * (c_one - c_mul * sky_light) +
-		sky_light * (c_one - c_mul * fire_light) +
-		ambient_light;
-}
-
 void main()
 {
-	vec4 tex_value= texture(texture_image, vec3(f_tex_coord, f_tex_index));
+	vec4 tex_value= HexagonFetch(texture_image, vec3(f_tex_coord, f_tex_index));
 
 	if(tex_value.a < 0.5)
 		discard;

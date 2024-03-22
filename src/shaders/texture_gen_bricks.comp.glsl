263c5185b78f564ade79407e44df1cc0492c7b4f
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-03-22 09:50:10 +0300

    Uzor kirpicnoj kladki sdelan pointeresneje.

--- src/shaders/texture_gen_bricks.comp.glsl
+++ src/shaders/texture_gen_bricks.comp.glsl
@@ -6,7 +6,7 @@
 #include "inc/texture_gen_common.glsl"
 
 const vec4 c_brick_color= vec4(0.65, 0.35, 0.32, 1.0);
-const vec4 c_mortar_color= vec4(0.843, 0.792, 0.792, 1.0);
+const vec4 c_mortar_color= vec4(0.67, 0.67, 0.67, 1.0);
 
 void main()
 {
@@ -17,7 +17,10 @@ void main()
 
 	bool is_horisontal_mortar= y_within_row == 0;
 
-	bool is_vertical_mortar= ((texel_coord.x + ((bricks_row & 1) << 3)) & 15) == 3;
+	int row_brick_size_log2= 4 + (bricks_row & 1); // Use different brick size in different rows.
+	int row_mask= (1 << row_brick_size_log2) - 1;
+	int column_offset= (bricks_row & 1) << 3;
+	bool is_vertical_mortar= ((texel_coord.x + column_offset) & row_mask) == 0;
 
 	vec4 color= (is_horisontal_mortar || is_vertical_mortar) ? c_mortar_color : c_brick_color;
 

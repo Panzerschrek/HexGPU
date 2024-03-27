bb6ff9d5e85b4b4885f178728b4a1b89ddac775a
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-03-27 15:42:12 +0300

    Nacato napisanije klassa dlä prohoda renderinga mira.

---
+++ src/WorldRenderPass.cpp
@@ -0,0 +1,108 @@
+#include "WorldRenderPass.hpp"
+#include "Log.hpp"
+#include "VulkanUtils.hpp"
+
+namespace HexGPU
+{
+
+namespace
+{
+
+vk::Extent3D GetFramebufferTextureSize(WindowVulkan& window_vulkan)
+{
+	const vk::Extent2D size_2d= window_vulkan.GetViewportSize();
+	return vk::Extent3D(size_2d.width, size_2d.height, 1);
+}
+
+vk::Format ChooseDepthFormat(const vk::PhysicalDevice physical_device)
+{
+	static constexpr vk::Format depth_formats[]
+	{
+		// Depth formats by priority.
+		vk::Format::eD32Sfloat,
+		vk::Format::eD24UnormS8Uint,
+		vk::Format::eX8D24UnormPack32,
+		vk::Format::eD32SfloatS8Uint,
+		vk::Format::eD16Unorm,
+		vk::Format::eD16UnormS8Uint,
+	};
+
+	for(const vk::Format depth_format_candidate : depth_formats)
+	{
+		const vk::FormatProperties format_properties=
+			physical_device.getFormatProperties(depth_format_candidate);
+
+		const vk::FormatFeatureFlags required_falgs= vk::FormatFeatureFlagBits::eDepthStencilAttachment;
+		if((format_properties.optimalTilingFeatures & required_falgs) == required_falgs)
+		{
+			Log::Info("Framebuffer depth format: ", vk::to_string(depth_format_candidate));
+			return depth_format_candidate;
+		}
+	}
+
+	Log::Warning("Can't choose proper depth format, using ", vk::to_string(vk::Format::eD16Unorm));
+	return vk::Format::eD16Unorm;
+}
+
+} // namespace
+
+WorldRenderPass::WorldRenderPass(WindowVulkan& window_vulkan)
+	: vk_device_(window_vulkan.GetVulkanDevice())
+	, framebuffer_size_(GetFramebufferTextureSize(window_vulkan))
+	, image_(vk_device_.createImageUnique(
+		vk::ImageCreateInfo(
+			vk::ImageCreateFlags(),
+			vk::ImageType::e2D,
+			vk::Format::eR8G8B8A8Unorm,
+			framebuffer_size_,
+			1u,
+			1u,
+			vk::SampleCountFlagBits::e1,
+			vk::ImageTiling::eOptimal,
+			vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eSampled,
+			vk::SharingMode::eExclusive,
+			0u, nullptr,
+			vk::ImageLayout::eUndefined)))
+	, image_memory_(AllocateAndBindImageMemory(vk_device_, *image_, window_vulkan.GetMemoryProperties()))
+	, image_view_(vk_device_.createImageViewUnique(
+		vk::ImageViewCreateInfo(
+			vk::ImageViewCreateFlags(),
+			*image_,
+			vk::ImageViewType::e2D,
+			vk::Format::eR8G8B8A8Unorm,
+			vk::ComponentMapping(),
+			vk::ImageSubresourceRange(vk::ImageAspectFlagBits::eColor, 0u, 1u, 0u, 1u))))
+	, depth_format_(ChooseDepthFormat(window_vulkan.GetPhysicalDevice()))
+	, depth_image_(vk_device_.createImageUnique(
+		vk::ImageCreateInfo(
+			vk::ImageCreateFlags(),
+			vk::ImageType::e2D,
+			depth_format_,
+			framebuffer_size_,
+			1u,
+			1u,
+			vk::SampleCountFlagBits::e1,
+			vk::ImageTiling::eOptimal,
+			vk::ImageUsageFlagBits::eDepthStencilAttachment | vk::ImageUsageFlagBits::eSampled,
+			vk::SharingMode::eExclusive,
+			0u, nullptr,
+			vk::ImageLayout::eUndefined)))
+	, depth_image_memory_(AllocateAndBindImageMemory(vk_device_, *depth_image_, window_vulkan.GetMemoryProperties()))
+	, depth_image_view_(vk_device_.createImageViewUnique(
+		vk::ImageViewCreateInfo(
+			vk::ImageViewCreateFlags(),
+			*depth_image_,
+			vk::ImageViewType::e2D,
+			depth_format_,
+			vk::ComponentMapping(),
+			vk::ImageSubresourceRange(vk::ImageAspectFlagBits::eDepth, 0u, 1u, 0u, 1u))))
+{
+}
+
+WorldRenderPass::~WorldRenderPass()
+{
+	// Sync before destruction.
+	vk_device_.waitIdle();
+}
+
+} // namespace HexGPU
---
+++ src/WorldRenderPass.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "WindowVulkan.hpp"
+
+namespace HexGPU
+{
+
+class WorldRenderPass
+{
+public:
+	explicit WorldRenderPass(WindowVulkan& window_vulkan);
+	~WorldRenderPass();
+
+private:
+	const vk::Device vk_device_;
+
+	const vk::Extent3D framebuffer_size_;
+
+	const vk::UniqueImage image_;
+	const vk::UniqueDeviceMemory image_memory_;
+	const vk::UniqueImageView image_view_;
+
+	const vk::Format depth_format_;
+	const vk::UniqueImage depth_image_;
+	const vk::UniqueDeviceMemory depth_image_memory_;
+	const vk::UniqueImageView depth_image_view_;
+};
+
+} // namespace HexGPU

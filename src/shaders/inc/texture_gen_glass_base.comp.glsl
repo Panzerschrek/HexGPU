b45ee7e396f363cdaf64c8186ceb15d8f2608bae
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-03-29 10:36:57 +0300

    Na teksture stekla ostavleny toljko vertikaljnyje polosy. Rasstojanije meždu nimi - 1/6 razmera tekstury, ctoby polosy šli po granäm blokov.

--- src/shaders/inc/texture_gen_glass_base.comp.glsl
+++ src/shaders/inc/texture_gen_glass_base.comp.glsl
@@ -5,17 +5,18 @@ void main()
 {
 	ivec2 texel_coord= ivec2(gl_GlobalInvocationID.xy);
 
-	ivec2 grid= texel_coord & 31;
-	ivec2 grid_cell= texel_coord >> 5;
-
 	vec2 texel_coord_f= vec2(float(texel_coord.x), float(texel_coord.y) - 0.5 * float(texel_coord.x & 1));
 
 	float frequency= c_pi / 16.0;
-	float s= sin((texel_coord_f.x + texel_coord_f.y * 2.0 + float(grid_cell.x * 14.0 + grid_cell.y * 7.0)) * frequency);
+	float s= sin((texel_coord_f.x + texel_coord_f.y * 2.0) * frequency);
 
 	vec4 color= vec4(GLASS_COLOR * (0.75 + 0.25 * s), 0.45 + 0.15 * s);
 
-	if(grid.x <= 1 || grid.y <= 1)
+	const float x_step= float(1 << c_texture_size_log2) / 6.0;
+
+	float grid_cell= float(texel_coord.x) / x_step;
+	float grid_cell_fract= fract(grid_cell);
+	if(grid_cell_fract < 1.0 / x_step || grid_cell_fract >= (1.0 - 1.0 / x_step))
 		color= vec4(GLASS_COLOR, 1.0);
 
 	imageStore(out_image, texel_coord, color);

22d4936c9e9e3a9e93c9074e4d89fe4d33186207
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-03-29 08:43:07 +0300

    Aljfa-kanal listvy sdelan binarnym.

--- src/shaders/texture_gen_foliage.comp.glsl
+++ src/shaders/texture_gen_foliage.comp.glsl
@@ -66,7 +66,7 @@ VoronoiPatternResult VoronoiPattern(ivec2 texel_coord, int seed_offset)
 	VoronoiPatternResult result;
 
 	float min_dist_diff= abs(second_min_dist - min_dist);
-	result.edge_factor= smoothstep(0.5 / float(c_cell_size), 1.0 / float(c_cell_size), min_dist_diff);
+	result.edge_factor= smoothstep(0.0, 1.75 / float(c_cell_size), min_dist_diff);
 
 	vec4 closest_cell_noise= vec4(
 		float(hex_Noise2(closest_cell_coord.x, closest_cell_coord.y, 8765 + seed_offset)),
@@ -91,6 +91,7 @@ void main()
 	vec4 background_color= vec4(background_pattern.cell_color * 0.5, background_pattern.edge_factor);
 	vec4 foreground_color= vec4(foreground_pattern.cell_color, foreground_pattern.edge_factor);
 	vec4 color= mix(background_color, foreground_color, foreground_color.a);
+	color.a= step(0.5, color.a);
 
 	imageStore(out_image, texel_coord, color);
 }

f190d4f710b2786f6bf99552e7cf9d47d911eca9
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-04-03 11:06:40 +0300

    Popravlena razmernostj vektora poziçii.

--- src/shaders/stars.vert.glsl
+++ src/shaders/stars.vert.glsl
@@ -9,7 +9,7 @@ layout(binding= 0) uniform uniforms_block_variable
 	SkyShaderUniforms uniforms;
 };
 
-layout(location=0) in vec4 pos;
+layout(location=0) in vec3 pos;
 layout(location=1) in vec4 color;
 
 layout(location=0) out vec4 f_color;
@@ -17,5 +17,5 @@ layout(location=0) out vec4 f_color;
 void main()
 {
 	f_color= color;
-	gl_Position= uniforms.view_matrix * vec4(pos.xyz, 1.0);
+	gl_Position= uniforms.view_matrix * vec4(pos, 1.0);
 }
